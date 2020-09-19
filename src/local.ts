7a676e796dd3d26e977c7e1108272f3e6ebbb09c
Danilo Mourelle <danilo@provi.com.br>
2020-09-18 23:40:18 -0300

    address

--- src/local.ts
+++ src/local.ts
@@ -1,13 +1,13 @@
 import app from "./index";
 import dotenv from 'dotenv'
-//import { AddressInfo } from "net";
+import { AddressInfo } from "net";
 
 dotenv.config()
 
 const server = app.listen(3003, () => {
   if (server) {
-    //const address = server.address() as AddressInfo;
-    console.log(`Servidor rodando em http://localhost:3003`);
+    const address = server.address() as AddressInfo;
+    console.log(`Servidor rodando em http://localhost:${address}`);
   } else {
     console.error(`Falha ao rodar o servidor.`);
   }
