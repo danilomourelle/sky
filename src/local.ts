8890b182f1570aebf258aac9053e84fbb21695e4
Danilo Mourelle <danilo@provi.com.br>
2020-09-18 23:44:32 -0300

    address

--- src/local.ts
+++ src/local.ts
@@ -4,7 +4,7 @@ import { AddressInfo } from "net";
 
 dotenv.config()
 
-const server = app.listen(3003, () => {
+const server = app.listen(5000, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Servidor rodando em http://localhost:${address}`);
